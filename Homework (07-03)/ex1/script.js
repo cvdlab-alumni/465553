dc983ee3b804b42e84ce60c7751afe885771339b
matpresidentric <mat.president.ric@hotmail.it>
2013-03-05 19:01:00 +0100

    Prima Versione Homework 1 del 07/03/2013

---
+++ Homework (07-03)/ex1/script.js
@@ -0,0 +1,11 @@
+var x='\n';
+var n = 10;
+var m = 10;
+for (var i = 0; i < n; i++) {
+  for (var j = 0; j < m; j++) {
+    x += (i+1)* (j+1);
+    x +='\t'
+  }
+  x += '\n';
+}
+console.log(x)
---
+++ Homework (07-03)/ex3/script.js
@@ -0,0 +1,15 @@
+var x='\n';
+var n = 10;
+var m = 10;
+for (var i = 0; i < n; i++) {
+ for (var j = 0; j < m; j++) {
+
+if((j)===(i))x+=1;
+   else x+=0;
+
+if((j+1)!==10)x +=',';
+    x +='\t'
+  }
+  x += '\n';
+}
+console.log(x)
